024ccd5b60cd35080d68616a7e00fc1d3369d1f2
Clevoh <clevelihasi.com>
2024-04-11 12:35:42 -0700

    task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,9 +4,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API
-    and returns the number of subscribers (not active users,
-    total subscribers) for a given subredditArgs:
+    """Queries the Reddit API and returns the number of subscribers (not
+    active users, total subscribers) for a given subreddit.
+
+    If not a valid subreddit, return 0.
+    Invalid subreddits may return a redirect to search results. Ensure that
+    you are not following redirects.
+
+    Args:
         subreddit (str): subreddit
 
     Returns:
