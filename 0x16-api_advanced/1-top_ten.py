a92f3c397c2d398912455073d907c534c5c745c7
Clevoh <clevelihasi@gmail.com>
2024-05-08 13:54:06 +0000

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
+Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
 """
 
 import requests
 
 
-def top_ten(subreddit):
+def number_of_subscribers(subreddit):
     """
     Function that queries the Reddit API
-    - If not a valid subreddit, print None.
+    - If not a valid subreddit, return 0.
     """
     req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Custom"},
-        params={"limit": 10},
     )
 
     if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
+        return req.json().get("data").get("subscribers")
     else:
-        print(None)
+        return 0
