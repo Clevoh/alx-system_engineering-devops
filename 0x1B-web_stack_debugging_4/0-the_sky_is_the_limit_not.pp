c61a81baf934816c6a41dbb1fe9c5db22c2d9c96
Clevoh <clevelihasi@gmail.com>
2024-05-16 03:25:27 +0000

    corrected

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,24 @@
 # Increase the amount of traffic nginx can handle
 
-#Increase the ULIMIT to default value
+# Increase the ULIMIT to default value
 exec { 'fix -- for nginx':
-# modify the ULIMIT value
-command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-# Specify the path for the sed command
-path => '/usr/local/bin/:/bin/',
+  # modify the ULIMIT value
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  # Specify the path for the sed command
+  path    => ['/usr/local/bin/', '/bin/'],
 }
 
-#Restart Nginx
-exec { 'nginx -restart':
-# Restart nginx services,
-command => '/etc/init.d/nginx restart',
-# Specify the path for init.d command
-path => '/etc/init.d',
+# Restart Nginx
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+}
+
+exec { 'nginx_restart':
+  # Restart nginx services,
+  command => '/etc/init.d/nginx restart',
+  # Specify the path for init.d command
+  path    => ['/etc/init.d'],
+  # Notify the Nginx service to restart when the exec runs
+  notify  => Service['nginx'],
 }
